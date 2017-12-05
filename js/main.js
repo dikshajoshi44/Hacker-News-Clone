85649018a5096bda3a67d7aceb72d4925f3ba59a
dikshajoshi44 <dikshajoshi131@gmail.com>
2017-12-05 16:29:55 +0530

    css file

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ $.getJSON( "http://localhost:8000/data/test.json", function( data ) {    //readi
 	  			'<br><br>' +
 	  			'<div class="ui circular label">' + value['score'] + '</div>' +
 	  			'<span class="ui tiny ">' + value['by'] + '</span>' +
-	  			'<div id = "dj">' + formatted + '</div>' +
+	  			'<span id = "dj">' + formatted + '</span>' +
 				'<span class="ui link ">' +
 				  '<b><a class="item">Comments </a> <span id="comm">' + value['descendants'] + '</span></b>' +
 				'</span>' +
